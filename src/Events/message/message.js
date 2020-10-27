c7dad8c703b412d1faa19de5d049e75139de19b3
EGGamer <eggamer131@gmail.com>
2020-10-27 16:32:05 +0100

    Fixed prefix

--- src/Events/message/message.js
+++ src/Events/message/message.js
@@ -3,15 +3,17 @@ const Event = require('../../structures/Event');
 module.exports = class extends Event {
 
 	async run(message) {
-		const mentionRegex = RegExp(`^<@!${this.client.user.id}>$`);
-		const mentionRegexPrefix = RegExp(`^<@!${this.client.user.id}> `);
+		const mentionRegex = RegExp(`^<@!?${this.client.user.id}>$`);
+		const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}> `);
 
-		if (!message.guild || message.author.bot) return;
+		if (message.author.bot) return;
 
 		if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${this.client.prefix}\`.`);
 
 		const prefix = message.content.match(mentionRegexPrefix) ?
 			message.content.match(mentionRegexPrefix)[0] : this.client.prefix;
+
+		if (!message.content.startsWith(prefix)) return;
 
 		const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
 
